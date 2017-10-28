0758b5e5ba84d834be68879940f23ba4203faebe
Nicholas Ricci <naricci@email.neit.edu>
2017-10-28 16:55:57 -0400

    add lab instructions 1 - 4 and lab1 project

---
+++ Lab1-XML/Lab1/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.lblVersionNumber = new System.Windows.Forms.Label();
+            this.txtVersionNumber = new System.Windows.Forms.TextBox();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.txtContacts = new System.Windows.Forms.TextBox();
+            this.lblContacts = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(43, 28);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(88, 24);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // lblVersionNumber
+            // 
+            this.lblVersionNumber.AutoSize = true;
+            this.lblVersionNumber.Location = new System.Drawing.Point(179, 35);
+            this.lblVersionNumber.Name = "lblVersionNumber";
+            this.lblVersionNumber.Size = new System.Drawing.Size(85, 13);
+            this.lblVersionNumber.TabIndex = 1;
+            this.lblVersionNumber.Text = "Version Number:";
+            // 
+            // txtVersionNumber
+            // 
+            this.txtVersionNumber.Location = new System.Drawing.Point(270, 32);
+            this.txtVersionNumber.Name = "txtVersionNumber";
+            this.txtVersionNumber.Size = new System.Drawing.Size(221, 20);
+            this.txtVersionNumber.TabIndex = 2;
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(270, 67);
+            this.txtFile.Multiline = true;
+            this.txtFile.Name = "txtFile";
+            this.txtFile.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtFile.Size = new System.Drawing.Size(221, 133);
+            this.txtFile.TabIndex = 3;
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Location = new System.Drawing.Point(179, 67);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(26, 13);
+            this.lblFile.TabIndex = 4;
+            this.lblFile.Text = "File:";
+            // 
+            // txtContacts
+            // 
+            this.txtContacts.Location = new System.Drawing.Point(270, 216);
+            this.txtContacts.Multiline = true;
+            this.txtContacts.Name = "txtContacts";
+            this.txtContacts.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtContacts.Size = new System.Drawing.Size(221, 133);
+            this.txtContacts.TabIndex = 5;
+            // 
+            // lblContacts
+            // 
+            this.lblContacts.AutoSize = true;
+            this.lblContacts.Location = new System.Drawing.Point(179, 219);
+            this.lblContacts.Name = "lblContacts";
+            this.lblContacts.Size = new System.Drawing.Size(52, 13);
+            this.lblContacts.TabIndex = 6;
+            this.lblContacts.Text = "Contacts:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 487);
+            this.Controls.Add(this.lblContacts);
+            this.Controls.Add(this.txtContacts);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.txtVersionNumber);
+            this.Controls.Add(this.lblVersionNumber);
+            this.Controls.Add(this.btnRun);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Label lblVersionNumber;
+        private System.Windows.Forms.TextBox txtVersionNumber;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.TextBox txtContacts;
+        private System.Windows.Forms.Label lblContacts;
+    }
+}
+
---
+++ Lab1-XML/Lab1/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            string strFilename = Application.StartupPath + "\\contacts.xml";
+            XmlTextReader objXMLTextReader = new XmlTextReader(strFilename);
+            string sName = "";
+            StringBuilder results = new StringBuilder();
+            StringBuilder vresults = new StringBuilder();
+            XmlDocument xmlDoc = new XmlDocument();
+
+            if (File.Exists(strFilename))
+            {
+                xmlDoc.Load(strFilename);
+                XmlElement elm = xmlDoc.DocumentElement;
+                XmlNodeList currentVersion = xmlDoc.GetElementsByTagName("CurrentVersion");
+                XmlNodeList contactInfo = elm.ChildNodes;
+
+                for (int iCount = 0; iCount < contactInfo.Count; iCount++)
+                {
+                    Console.WriteLine(contactInfo[0].Name);
+                    Console.WriteLine(contactInfo[0].FirstChild.Name + " - " + contactInfo[0].FirstChild.InnerText);
+
+                    XmlNodeList contactProperties = contactInfo[0].ChildNodes;
+                    for (int jCount = 0; jCount < contactProperties.Count; jCount++)
+                    {
+                        //todo FIX THIS PART!!!
+                        txtContacts.Text = contactProperties.ToString();
+                    }
+                }
+
+                // Check for each node of the list
+                foreach (XmlNode node in currentVersion)
+                {
+                    Console.WriteLine(node.InnerText);
+                    vresults.AppendLine(node.InnerText);
+                }
+
+                txtVersionNumber.Text = vresults.ToString();
+            }
+
+            while (objXMLTextReader.Read())
+            {
+                switch (objXMLTextReader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        sName = objXMLTextReader.Name;
+                        break;
+                    case XmlNodeType.Text:
+                        Console.WriteLine(sName + "-" + objXMLTextReader.Value);
+                        results.AppendLine(sName + "-" + objXMLTextReader.Value);
+                        break;
+
+                }
+            }
+
+            txtFile.Text = results.ToString();
+
+
+        }
+    }
+}
